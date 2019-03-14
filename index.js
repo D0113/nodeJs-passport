1b359c61ad6b2dae0f472929d08cdf447c5e08a6
DDD <MrD@example.com>
2019-03-14 23:01:41 +0700

    init project

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const port = 3000;
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.set(express.urlencoded({extended: true}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    passport.authenticate('local', { failureRedirect: '/login' });
+});
+
+app.listen(port, () => {
+    console.log('Server listen port', port);
+});
